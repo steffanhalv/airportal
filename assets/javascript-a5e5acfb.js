292892b43d5e8964ca34d57c60973c51eaf52549
steffanhalv <steffanhalv@users.noreply.github.com>
2023-07-02 22:35:14 +0000

    deploy: 7b2daac4562fc98af71af0972c833ca78ba51286

--- assets/javascript-a5e5acfb.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-37760907.js";import"./Type-34ae418a.js";import"./index-6a7df3d6.js";import"./moment-fbc5633a.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.36.1(6c56744c3419458f0dd48864520b759d1a3a1ca8)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var a=t,n={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{a as conf,n as language};

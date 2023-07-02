d77e6a2317970aa9d5bf5e3e59cc491a8a458d66
Vue Play <steffanhalv@gmail.com>
2023-07-02 23:52:19 +0200

    Stored by Vue Play 1688334739454

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,10 @@
 <template>
     <div style="font-family:Arial, Helvetica, sans-serif">
-        <div style="width:100%;height:100%;overflow:auto;background-color:#edebebff">
+        <div style="width:100%;height:100%;overflow:auto">
             <div style="margin-left:20px;margin-right:20px;margin-top:20px;margin-bottom:20px">
                 <h2 class="text-4xl mb-4">
                     Profile
-                </h2> <input type="" name="" /><button type="">
-                    Filter
-                </button>
+                </h2>
             </div>
             <div style="box-sizing:border-box;width:calc(100% - 40px);padding-left:20px;padding-right:20px;padding-top:20px;padding-bottom:20px;margin-left:20px;margin-right:20px;margin-top:20px;margin-bottom:20px;border-top-left-radius:3px;border-top-right-radius:3px;border-bottom-left-radius:3px;border-bottom-right-radius:3px;background-color:#83c9b3ff">
                 <div style="display:inline-block">
@@ -14,7 +12,7 @@
                         {{user?.value?.email || user?.email}} {{user?.value?.permissions || user?.permissions}}
                     </span>
                 </div>
-                <pre>
+                <pre class="bg-slate-800 text-slate-50 p-4 rounded shadow mt-4">
                 {{user}}
                 </pre>
             </div>

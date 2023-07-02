62d6e9eb3b4727c1ecb92768b6584f0af5945fa5
Vue Play <steffanhalv@gmail.com>
2023-07-03 01:25:26 +0200

    Stored by Vue Play 1688340326812

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,11 @@ export default {
     },
     plugins: [],
     safelist: [
-        'col-span-1',
-        'col-span-2',
-        'col-span-3',
-        'col-span-4',
-        'col-span-5',
-        'col-span-6',
-        'col-span-7',
-        'col-span-8',
-        'col-span-9',
-        'grid-col-1',
-        'grid-col-2',
-        'grid-col-3',
-        'grid-col-4',
-        'grid-col-5',
-        'grid-col-6',
-        'grid-col-7',
-        'grid-col-8',
-        'grid-col-9'
+        {
+            pattern: /grid-cols-./,
+        },
+        {
+            pattern: /col-span-./,
+        }
     ]
 }

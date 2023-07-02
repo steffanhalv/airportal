9cafbc3684bac8a81d0e4f0c6e9b6b4c2a95ea9f
Vue Play <steffanhalv@gmail.com>
2023-07-03 00:26:51 +0200

    Stored by Vue Play 1688336811719

--- src/components/InputUniversal.vue
+++ src/components/InputUniversal.vue
@@ -1,27 +1,26 @@
 <template>
-    <input v-if="typeof val !== 'object'" v-model="val" :placeholder="placeholder">
-    <input v-else readonly :value="JSON.stringify(val)" :placeholder="placeholder">
+    <input v-if="typeof value !== 'object'" v-model="value" :placeholder="placeholder">
+    <input v-else readonly :value="JSON.stringify(value)" :placeholder="placeholder">
 </template>
 
 <script>
-    export default {
-        props: ['modelValue', 'placeholder'],
-        data: () => ({
-            value: ''
-        }),
-        created() {
-            this.value = this.modelValue
+export default {
+    props: ['modelValue', 'placeholder'],
+    data: () => ({
+        value: ''
+    }),
+    created() {
+        this.value = this.modelValue
+    },
+    watch: {
+        modelValue(value) {
+            if (value !== this.value) this.value = value
         },
-        watch: {
-            modelValue(value) {
-                if (value !== this.value) this.value = value
-            },
-            value(value) {
-                this.$emit('update:modelValue', value)
-            }
+        value(value) {
+            this.$emit('update:modelValue', value)
         }
     }
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>

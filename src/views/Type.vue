139831cdd12942d3e462d437bf0ec86a6b541dc4
Vue Play <steffanhalv@gmail.com>
2023-07-03 11:58:31 +0200

    Stored by Vue Play 1688378311835

--- src/views/Type.vue
+++ src/views/Type.vue
@@ -21,7 +21,8 @@
                     Custom editor
                 </button><button @click="view='type'" class="disabled:opacity-25 cursor-pointer hover:text-slate-100 hover:bg-slate-400 shadow-slate-300 shadow rounded py-2 text-slate-700 bg-slate-300 dark mx-0.5 mr-1 px-3 mb-2" :disabled="view==='type'">
                     Type
-                </button><input :value="moment(instance.updatedAt).format('DD.MM.YY HH:mm:ss')" class="w-40 py-2 px-2 mx-0.5 rounded shadow-slate-200 shadow text-slate-700 mb-2" /> <button class="disabled:opacity-25 cursor-pointer shadow-slate-300 shadow rounded py-2 text-emerald-50 hover:text-white bg-emerald-400 hover:bg-emerald-500 px-3 mb-2" @click="saveType()"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mx-auto">
+                </button><input :value="moment(instance.updatedAt).format('DD.MM.YY HH:mm:ss')" class="w-40 py-2 px-2 mx-0.5 rounded shadow-slate-200 shadow text-slate-700 mb-2" /><button class="disabled:opacity-25 cursor-pointer shadow-slate-400 shadow rounded py-2 text-emerald-50 hover:text-white bg-emerald-400 hover:bg-emerald-500 px-3 mb-2 ml-1" @click="saveType()">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 inline-block">
                         <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
                     </svg> </button>
             </div>
